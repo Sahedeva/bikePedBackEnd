6c423630e7af29ece8a01ef388af8373d3d2531f
danielrobertson <danielrobertson733@gmail.com>
2016-06-04 15:47:20 -0500

    formatting

--- routes/index.js
+++ routes/index.js
@@ -7,47 +7,27 @@ var User = require('../models/user');
 var Route = require('../models/route'); 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Street Life, Saves Lives'});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Street Life, Saves Lives' });
 });
 
-
-router.post('/test', function (req, res, next) {
-    // Create a new user
-    var buddy = new User({
-        name: 'Buddy',
-        email: 'buddy@thenorthpole.com',
-        favorite: 'Smiling'
-    });
-    // Save the user
-    buddy.save(function (err) {
-        if (err) console.log(err);
-
-        res.send('User created!');
-    });
+/* GET map page. */
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Street Life, Saves Lives' });
 });
 
-router.post('/new', function (req, res, next) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var favorite = req.body.favorite;
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Street Life, Saves Lives' });
+});
 
-    var newUser = User({
-        name: name,
-        email: email,
-        favorite: favorite,
-    });
-
-    // Save the user
-    newUser.save(function (err) {
-        if (err) console.log(err);
-
-        res.send('User created!');
-    });
+/* GET googleMaps page. */
+router.get('/googleMaps', function(req, res, next) {
+  res.render('googleMaps', { title: 'Street Life, Saves Lives' });
 });
 
 /**
- * Write a route to the database, which consists of a user and list of location points
+ * POST a route collection, which consists of a userid and list of location points
  */
 router.post('/route', function (req, res, next) {    
     var route = new Route({
@@ -64,10 +44,50 @@ router.post('/route', function (req, res, next) {
     });
 });
 
-router.post("/postme", function(request, response) {
-  console.log("body: " + JSON.stringify(request.body)); 
-  response.send(JSON.stringify(request.body)); 
-}); 
+router.post('/test', function(req, res, next) {
+  // Create a new user 
+  var buddy = new User({
+    name: 'Buddy',
+    email: 'buddy@thenorthpole.com',
+    favorite: 'Smiling'
+  });
+  // Save the user
+  buddy.save(function(err) {
+    if (err) console.log(err);
+    User.findOne({
+        name: 'Buddy'
+      }, function(err, user) {
+        if (err) console.log(err);
+        console.log(user._id);
+        res.send(user._id);
+      });
+  });
+});
+
+router.post('/new', function(req, res, next) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var favorite = req.body.favorite;     
+
+    var newUser = User({
+        name: name,
+        email: email,
+        favorite: favorite,
+    });
+
+    // Save the user
+    newUser.save(function(err) {
+      if (err) console.log(err);
+
+      User.findOne({
+        name: name
+      }, function(err, user) {
+        if (err) console.log(err);
+        console.log(user._id);
+        res.send(user._id);
+      });
+    });
+});
 
 
 module.exports = router;
